068f0d91462a71de5a135ea297f28dfbcb91a50f
Paulo Mateus <paulomatew@gmail.com>
2018-09-17 21:44:00 -0300

    update path

--- IAproject/data/viewer/data/conversor.bat
+++ IAproject/data/viewer/data/conversor.bat
@@ -1,2 +1,2 @@
-java -jar %cd%/data/viewer/data/owl2vowl.jar -file ./data/viewer/data/foaf.owl -echo >> ./data/viewer/data/foaf.json
+java -jar %cd%/data/viewer/data/owl2vowl.jar -file %cd%/data/viewer/data/foaf.owl -echo >> %cd%/data/viewer/data/foaf.json
 exit
